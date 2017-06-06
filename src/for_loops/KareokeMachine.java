3835e7ce7c206558f99da12c10784c46a7515cb9
Danny <league@WorthyMini-6.attlocal.net>
2017-06-05 19:54:55 -0700

    teacher did not help me well!

---
+++ src/for_loops/KareokeMachine.java
@@ -0,0 +1,45 @@
+/*
+ * KareokeMachine
+ * BieberFever
+ *
+ * 1)Write a program that uses a for loop to print 
+ *   baby
+ *   baby
+ *   baby
+ *   oh
+ * Code should only contain one “baby”, and one “oh”
+ * 2) Every 2nd time the “oh” should be “no”
+ * 3) Each chorus ends with “I thought you'd always be mine”.
+ */
+package for_loops;
+
+import javax.swing.JOptionPane;
+
+public class KareokeMachine {
+public static void main(String[] args) {
+	
+	for(int i = 1; i < 5; i++){
+		if(i != 4){
+		System.out.println("Baby");
+		}
+		if(i == 4){
+		System.out.println("oh");
+		} 
+		
+		if()
+		
+	}
+	
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+}
+}
